7aa7015da173f7660ecb7424ed7acb11a324ef22
Hedvika <hedvika.drechsler@seznam.cz>
2024-05-21 17:10:13 +0200

    ukol 4 hotov

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,10 +1,13 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import cz.czechitas.java2webapps.ukol3.service.VizitkaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
@@ -29,5 +32,21 @@ public class VizitkaController {
         ModelAndView result = new ModelAndView("detail");
         result.addObject("vizitka", service.getById(id));
         return result;
+    }
+    @GetMapping("/nova")
+    public ModelAndView nova() {
+        ModelAndView result = new ModelAndView("nova");
+        return result;
+    }
+
+    @PostMapping("/nova")
+    public String append(Vizitka vizitka) {
+        service.append(vizitka);
+        return "redirect:/";
+    }
+    @PostMapping("/delete")
+    public String delete(int id) {
+        service.deleteById(id);
+        return "redirect:/";
     }
 }
